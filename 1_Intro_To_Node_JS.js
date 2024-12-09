13fb83b00b351f310a06283be4df3f659801f40f
bhavishaya-khandelwal-dianapps <bhavishaya.khandelwal@dianapps.com>
2024-12-09 17:24:49 +0530

    1_Intro_To_Node_JS

---
+++ 1_Intro_To_Node_JS.js
@@ -0,0 +1,113 @@
+//*=======================================
+//* What is Node JS ?
+//*=======================================
+//? It is open source, server-side JavaScript Runtime.
+
+//? Allows you to run JavaScript on your machine or servers. 
+
+//? Built on the V8 JavaScript for speed.
+
+//? Robust ecosystem with npm 
+
+
+
+
+
+
+
+//*=======================================
+//* What is Open Source ?
+//*=======================================
+//? Open Source means that the source code of Node.js is publicly available. 
+
+//? Anyone can :- 
+    //* View : You can see how Node.js is built under the hood .
+    
+    //* Modify : If you want to customize or improve Nde.js, you can do it. 
+
+    //* Contribute : Developers around the world actively contribute to improving Node.js
+
+
+
+
+
+
+
+//*=======================================
+//* What is Cross-Platform ?
+//*=======================================
+//? Cross platform means Node.js can run on multiple operating systems without needing major changes. 
+//* Windows, MAC, Linux 
+
+
+
+
+
+
+
+
+//*=======================================
+//* What is JavaScript Runtime Environment ?
+//*=======================================
+//? JavaScript runtime environment refers to the environment where your JavaScript code runs. 
+
+//* In browers : JavaScript typically runs in the browser (like Chrome or FireFox) to handle frontend tasks. 
+
+//? With Node.js :- 
+//* Node.js allows JS to run outside the browser, on the server. 
+//? It provides tools to interact with the system, like : 
+    //! File System (read, write files) 
+    //! Network (handle HTTP files) 
+    //! Database (connect to databases like MongoDB or MySQL) 
+
+
+
+
+
+
+
+
+//*=======================================
+//* Why is Node.js Special as a Runtime ?
+//*=======================================
+//? V8 Engine :
+    //* Node.js uses Google Chrome's V8 engine to compile JavaScript into machine code, making it lighting-fast. 
+
+//? Built-in APIs : 
+    //* Node.js comes with built-in APIs (like fs for file systems or http for server) so you can build powerful applications without extra libraries. 
+
+
+
+
+
+
+
+
+//*=======================================
+//* Why do we need Node.js ?
+//*=======================================
+//! Single language for full stack development :- 
+    //? Developers can use JS for both frontend and backend, reducing complexity.
+    //? For example, a MERN stack project (MongoDB, Express, React, Node)
+
+//! High Performance :- 
+    //? Thanks to the V8 engine, Node.js is super fast. 
+    //? It can handle thousands of simultaneous connections with ease. 
+
+//! Event-Driven and Non-Blocking I/O :- 
+    //? Unlike traditional servers that block a thread for each request, Node.js uses an asynchronous model, making it highly efficient for handling multiple tasks. 
+
+//! Scalable for Modern Applications :- 
+    //? Ideal for real-time applications like chat apps, streaming platforms, or online games. 
+
+
+
+
+
+
+
+
+//*=======================================
+//* Is it a language or a Framework ?
+//*=======================================
+//? Node.js is not a programming language or a framework, but rather a JavaScript Runtime Environment that allows developers to run JavaScript outside of a browser. 
