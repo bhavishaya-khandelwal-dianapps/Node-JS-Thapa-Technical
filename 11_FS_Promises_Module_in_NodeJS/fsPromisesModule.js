5e83a788cdee2dc2f9d4a245b79209735cabacd4
bhavishaya-khandelwal-dianapps <bhavishaya.khandelwal@dianapps.com>
2024-12-12 16:42:32 +0530

    File System Module Completed

---
+++ 11_FS_Promises_Module_in_NodeJS/fsPromisesModule.js
@@ -0,0 +1,117 @@
+//*====================================
+//* FS PROMISES MODULE - NODE JS :- 
+//*====================================
+//? The fs/promises module provides a promise-based API for interacting with the filesystem, allowing asynchronous operations to be handled using modern JavaScript features like async/await or .then() chains. It is a part of the fs module in Node.js but designed for those who prefer promises over callback-based or synchronous methods. 
+
+
+
+
+//* Purpose :- asynchronous file operations by using promises 
+//? Simplifies asynchronous file operations by using promises and making the code more readable and modern compared to traditional callback-based approaches.
+
+
+
+//* Advantages :- 
+//? NO need for manual callbacks. 
+//? Cleaner and more intuitive asynchronous workflows using async/await. 
+//? Suitable for modern JavaScript applications. 
+
+
+
+//* Use Cases :- 
+//? Reading Files asynchronously in a non-blocking way. 
+//? Writing or Appending Data to files without blocking the event loop. 
+//? Performing Multiple File Operations sequentially or concurrently with promise chaining. 
+//? Handling Errors Gracefully with .catch() or try....catch blocks.
+
+
+
+const fs = require("fs");
+// const fs = require("fs/promises");
+const path = require("path");
+
+const fileName = "fsPromises.txt";
+const filePath = path.join(__dirname, fileName);
+
+
+
+
+
+//! To read files in a the current directory 
+const currentDirectory = __dirname;
+fs.promises.readdir(currentDirectory)
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.log(error);
+})
+
+
+
+
+
+
+
+
+
+//! To create a new file 
+// fs.promises.writeFile(filePath, "Initail Data", "utf-8")
+// .then((data) => {
+//     console.log("File Creted Successfully");
+// })
+// .catch((err) => {
+//     console.log(err);
+// })
+
+
+
+
+
+
+
+//! To read data from that file 
+// fs.promises.readFile(filePath, "utf-8")
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((error) => {
+//     console.log(error);
+// })
+
+
+
+
+
+
+
+
+//! To append some more data in the created file 
+// fs.promises.appendFile(filePath, "\nExtra data", "utf-8")
+// .then((data) => {
+//     console.log("Data Added Successfully");
+// })
+// .catch((error) => {
+//     console.log(error);
+// })
+
+
+
+
+
+
+
+//! To delete the created file 
+// fs.promises.unlink(filePath)
+// .then(() => {
+//     console.log("FIle deleted successfully");
+// })
+// .catch((error) => {
+//     console.log(error);
+// })
+
+
+
+
+
+
---
+++ 12_FS_Async_Await_Module_In_NodeJS/fsAsyncAwaitModuleInNodeJS.js
@@ -0,0 +1,95 @@
+//*======================================
+//* FS ASYNC AWAIT MODULE - NODE JS :- 
+//*======================================
+
+const fs = require("fs");
+const path = require("path");
+
+
+const fileName = "asyncFile.txt";
+const filePath = path.join(__dirname, fileName);
+
+
+
+
+
+
+
+
+//! To get all the files in the current directory
+const readFolder = async () => {
+    try {
+        const res = await fs.promises.readdir(__dirname);
+        console.log(res);
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+// readFolder();
+
+
+
+
+
+
+
+
+
+//! Create a new file 
+const writeFileExample = async () => {
+    try {
+        await fs.promises.writeFile(filePath, "Initial Data is nothing", "utf-8");
+        console.log("FIle is created");
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+
+// writeFileExample();
+
+
+
+
+
+
+
+
+//! Append some more data 
+const appendData = async () => {
+    try {
+        await fs.promises.appendFile(filePath, "\nSome more data", "utf-8");
+        console.log("Data Added Successfully");
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+
+// appendData();
+
+
+
+
+
+
+
+
+
+//! To delete that file 
+const deleteTheCreatedFile = async () => {
+    try {
+        await fs.promises.unlink(filePath);
+        console.log("File Deleted successfully");
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+// deleteTheCreatedFile();
+
+
+
+
+
