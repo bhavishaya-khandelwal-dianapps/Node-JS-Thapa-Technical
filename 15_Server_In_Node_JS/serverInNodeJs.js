9df01a9b58926e83bf128c04b04cbd93c10fbb46
bhavishaya-khandelwal-dianapps <bhavishaya.khandelwal@dianapps.com>
2024-12-12 17:46:42 +0530

    HTTP Modules in Node JS

---
+++ 15_Server_In_Node_JS/serverInNodeJs.js
@@ -0,0 +1,10 @@
+//*=======================================
+//* HTTP Module - Node JS :- 
+//*=======================================
+//? The http module in Node.js allows developers to create an HTTP server and handle client requests and server responses. It provides methods and properties to work with HTTP requests and responses, enabling the creation of REST APIs, web pages, and other networked applications.
+
+
+
+
+
+//? A web server is software or hardware that serves web content (HTML, CSS, JavaScript, etc.) to clients (usually browsers) over the interbet or an intranet. It uses protocols like HTTP/HTTPS to handle requests and responses. 
