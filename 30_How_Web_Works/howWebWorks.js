be53be4397443b3ee6f0b2b796ffd266bd1491d5
bhavishaya-khandelwal-dianapps <bhavishaya.khandelwal@dianapps.com>
2024-12-31 12:35:12 +0530

    How Web Works

---
+++ 30_How_Web_Works/howWebWorks.js
@@ -0,0 +1,10 @@
+//* HTTP and HTTPS :- 
+//? HTTP stands for Hyper Text Transfer Protocol and is used for transmitting data over the web. 
+
+//? It defines how requests and responses should look and how the server should handle them
+
+//? HTTPS is the secure version of HTTP, using SSL/TLS encryption to protect data during transmission. 
+
+//? The browser sends an HTTP/HTTPS request to the server for a specific resource (e.g., webpage, image).
+
+//? There are different HTTP methods like GET, POST, PUT, DELETE, PATCH, etc. which we will learn more in detail in future sections.
